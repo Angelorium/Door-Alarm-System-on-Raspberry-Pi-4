e645ea5b7d65a3f39bcf4e8786182e66f53c3b8e
Angelorium <angelobarbera01@gmail.com>
2023-01-25 22:40:58 +0100

    added merge script

---
+++ merge.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+cd src/
+cat se-ans.f utils.f i2c.f |
+awk -F"\\" '{print $1}' |  				             
+awk -F"[^A-Z]+[()][^A-Z]+" '{print $1 $    3}' | 	 
+awk '{ printf "%s ", $0 }' |                         
+sed 's/\;/\;\n/g' |                                 
+sed '/^[[:space:]]*$/d' |				             
+sed -e 's/\t/ /g' | tr -s ' ' > merged_src.f  	 
