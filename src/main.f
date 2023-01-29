92d353a4e4c2f52287ea5b8154f396a4891a4a3d
Angelorium <angelo.barbera01@gmail.com>
2023-01-29 19:23:11 +0100

    updated main

--- src/main.f
+++ src/main.f
@@ -2,5 +2,23 @@ HEX
 
 : RUN ( -- )
     INITIALIZE_LCD
+    CLEAR SEND_COMMAND
     INITIALIZE_PERIPHERALS
+    GREEN_LED ON 
+    
+    BEGIN 
+        HALL_SENSOR_STATUS 1 = IF 
+            GREEN_LED OFF 
+            RED_LED ON 
+            BUZZER ON
+            CLEAR SEND_COMMAND
+            DOOR OPEN SEND_WORD SEND_WORD
+        THEN
+        BUTTON_STATUS 1 = HALL_SENSOR_STATUS 0 = AND IF 
+            RED_LED OFF 
+            GREEN_LED ON 
+            BUZZER OFF
+            CLEAR SEND_COMMAND
+            DOOR CLOSED SEND_WORD SEND_WORD 
+     UNTIL
 ;
